830299e3e3c6a13175cb39c9453fc1c96674f514
Tiago329 <95241788+Tiago329@users.noreply.github.com>
2023-02-09 19:43:48 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import React from 'react'
 import Head from 'next/head'
-import Main from '../Components/Main'
 import Hero from '../Components/Hero'
 import Faq from '../Components/Faq'
 import Team from '../Components/Team'
