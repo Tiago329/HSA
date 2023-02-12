ef3a35f4ccdc0423af2a9638517cca3a1be32774
Tiago329 <95241788+Tiago329@users.noreply.github.com>
2023-02-12 19:00:57 +0000

    update4

--- Components/Team.jsx
+++ Components/Team.jsx
@@ -11,7 +11,7 @@ const Team = () => {
         <div class="grid gap-12 items-center md:grid-cols-3">
             <div class="space-y-4 text-center">
                 <img class="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64" 
-                    src="/banana2.jpg" alt="woman" loading="lazy" width="640" height="805"/>
+                    src="/tiagoCEO.jpg" alt="woman" loading="lazy" width="640" height="805"/>
                 <div>
                     <h4 class="text-2xl">Tiago Marques</h4>
                     <span class="block text-sm text-gray-500">CEO-Founder</span>
@@ -19,7 +19,7 @@ const Team = () => {
             </div>
             <div class="space-y-4 text-center">
                 <img class="w-64 h-64 mx-auto object-cover rounded-xl md:w-48 md:h-64 lg:w-64 lg:h-80" 
-                    src="/banana2.jpg" alt="man" loading="lazy" width="1000" height="667"/>
+                    src="/pedroArtist.png" alt="man" loading="lazy" width="1000" height="667"/>
                 <div>
                     <h4 class="text-2xl">Pedro Ferreira</h4>
                     <span class="block text-sm text-gray-500">Chief Technical Artist</span>
